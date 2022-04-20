9508def38786ab5dd1031a6bb20e7e0fb8cab06e
Aly Kane <Aly@macbook-air.myfiosgateway.com>
2022-04-20 16:16:50 -0400

    Created basic form for CreatePost file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
+import CreatePost from './Components/CreatePost/CreatePost'
 
 function App() {
 
-  const[posts, setPosts] = useState({Name: 'Aly', Post: "I heart learning"})
+  const [posts, setPosts] = useState()
+
+  function addNewPost(post){
+    let tempPosts = [...posts, post];
+    setPosts(tempPosts);
+  }
 
 
   return (
     <div>
-     <h3>Hi friends</h3>
+     <h3>SocialFeed</h3>
+        <div>
+        <CreatePost createNewPost={addNewPost} />
+        </div>
+  
       
     </div>
   );
---
+++ src/Components/CreatePost/CreatePost.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+
+
+const CreatePost = (props) => {
+
+const [name, setName] = useState('');
+const [post, setPost] = useState('');
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let newPost = {
+        name: name,
+        post: post
+    };
+    console.log(newPost)
+    props.createNewPost(newPost)
+}
+
+    return (  
+        <form onSubmit={handleSubmit} >
+            <div>
+                <label>Name</label>
+                <input type="text"  value={name} onChange={(event) => setName(event.target.value)}/>
+            </div>
+            <div>
+                <label>Post</label>
+                <input type="text" value={post} onChange={(event) => setPost(event.target.value)}/>
+            </div>
+            <button type='submit'>Create</button>
+        </form>
+    );
+}
+
+export default CreatePost;
